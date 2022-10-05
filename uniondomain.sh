69a8a09cb8e9c982c0936f7a64c39ee4030b471d
Rizaldy Christoffel <31089448+fireorb@users.noreply.github.com>
2022-10-05 18:57:15 +0800

    Update uniondomain.sh

--- uniondomain.sh
+++ uniondomain.sh
@@ -8,3 +8,17 @@ echo "HiddenServiceDir /var/lib/tor/hidden_service/" >> /etc/tor/torrc
 echo "HiddenServicePort 80 127.0.0.1:80" >> /etc/tor/torrc
 service tor restart
 cat /var/lib/tor/hidden_service/hostname
+
+
+#/bin/bash
+
+sudo apt update -y
+sudo apt install apache2 apt-transport-https -y
+sudo service apache2 start
+sudo apt install tor -y
+sudo service tor start
+sudo bash -c 'echo "HiddenServiceDir /var/lib/tor/hidden_service/" >> /etc/tor/torrc'
+sudo bash -c 'echo "HiddenServicePort 80 127.0.0.1:80" >> /etc/tor/torrc'
+sudo service tor restart
+sleep 3
+sudo bash -c 'cat /var/lib/tor/hidden_service/hostname'
